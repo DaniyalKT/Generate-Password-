eea68e648625a579012198d8bbe4be964fa0f09d
DaniyalKT <ktdaniyal76@gmail.com>
2022-11-26 16:55:56 +0330

    on ts file...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   Password: string
+  ChangeNumbers: boolean = false
+  ChangeLatter: boolean = false
+  ChangeSymbols: boolean = false
+  lenght: number = 0
 
-  constructor(){
+  constructor() {
     this.Password = ''
   }
 
   OnButtonClick() {
-    this.Password = "Your Password!"
+    debugger
+    const Numbers = '0123456789'
+    const latters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()_+/~'
+    let validChar = ''
+    if (this.ChangeNumbers) {
+      validChar += Numbers
+    }
+    if (this.ChangeLatter) {
+      validChar += latters
+    }
+    if (this.ChangeSymbols) {
+      validChar += symbols
+    }
+    let generatedPassword = ''
+    for (let index = 0; index < this.lenght; index++) {
+      let index = Math.floor(Math.random() * validChar.length)
+      generatedPassword += validChar[index]
+    }
+    this.Password += generatedPassword
   }
 
-  getPassword(){
-    return this.Password
+  onChangeNumbers() {
+    this.ChangeNumbers = !this.ChangeNumbers
   }
+
+  onChangeSymbols() {
+    this.ChangeSymbols = !this.ChangeSymbols
+  }
+
+  onChangeLatter() {
+    this.ChangeLatter = !this.ChangeLatter
+  }
+
+  onChangeLenght(value: any) {
+    const resultValue = (value.target as HTMLInputElement).value
+    const parsedValue = parseInt(resultValue)
+    if (!isNaN(parsedValue)) {
+      this.lenght = parsedValue
+    }
+  }
+
 }
